becb8c865f32d3cab0c2d8867e59d7eba706d550
Your Name <vladedumail@gmail.com>
2020-10-22 12:29:21 +0300

    Files front build folder replaced to root folder for github-pages branch

---
+++ precache-manifest.2c9b02f25d6b766c36e62dcf0ea24aca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a72c659fc796b893c477031e3a1511",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe7f5b9428ce16a7dab4",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "fb3f0493f98a55139642",
+    "url": "/static/css/main.98449132.chunk.css"
+  },
+  {
+    "revision": "fe7f5b9428ce16a7dab4",
+    "url": "/static/js/2.091b0b60.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.091b0b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb3f0493f98a55139642",
+    "url": "/static/js/main.a65154a2.chunk.js"
+  },
+  {
+    "revision": "7115e8cd09f3c023212b",
+    "url": "/static/js/runtime-main.ebda1548.js"
+  }
+]);
